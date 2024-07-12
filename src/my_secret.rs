54da150570369137501a01ef502d820371f711bc
hversu <101164411+hversu@users.noreply.github.com>
2024-07-12 01:10:40 -0400

    Update my_secret.rs

--- src/my_secret.rs
+++ src/my_secret.rs
@@ -1,2 +1,2 @@
-pub const SERP_API_KEY: &str = "<get a fre key at serpapi.com>"; 
+pub const SERP_API_KEY: &str = "<get a free key at serpapi.com>"; 
 pub const OPENAI_KEY: &str = "<your OpenAI key>";
